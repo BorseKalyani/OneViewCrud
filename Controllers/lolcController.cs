28f63651c6f4605792e7dbfbd35506714f5ee4a6
kalyani borse <kalyaniborse48@gmail.com>
2023-04-25 18:49:04 +0530

    Add project files.

---
+++ Controllers/lolcController.cs
@@ -0,0 +1,74 @@
+﻿using OneViewCrud.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+
+namespace OneViewCrud.Controllers
+{
+    public class lolcController : Controller
+    {
+        DemoEntities db = new DemoEntities();
+        // GET: lolc
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult save(record rec)
+        {
+            bool status = false;
+            if (ModelState.IsValid ==true)
+            {
+                if (rec.id > 0)
+                {
+                    var v = db.records.Where(x => x.id == rec.id).FirstOrDefault();
+                    if (v != null)
+                    {
+                        v.coursename = rec.coursename;
+                        v.fee = rec.fee;
+                        db.Entry(v).State = EntityState.Modified;
+                    }
+                }
+                else
+                {
+                    db.records.Add(rec);
+
+                }
+                    db.SaveChanges();       
+                    status = true;
+
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "<script>alert('Enter UserName or Fee')</script>";
+            }
+            return new JsonResult { Data = new { status = status } };
+        }
+        public ActionResult GetCourses()
+        {
+            var courses = db.records.ToList();
+            return Json(new { data = courses }, JsonRequestBehavior.AllowGet);
+
+        }
+        [HttpGet]
+        public JsonResult Edit(int Id)
+        {
+            var course = db.records.Where(x => x.id == Id).FirstOrDefault();
+            return Json(course, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult Remove(int Id)
+        {
+            bool status = false;
+            var course = db.records.FirstOrDefault(x => x.id == Id);
+            if (course != null)
+            {
+                db.records.Remove(course);
+                db.SaveChanges();
+                status = true;
+            }
+            return new JsonResult { Data = new { status = status } };
+        }
+    }
+}
---
+++ Scripts/coding-function.js
@@ -0,0 +1,108 @@
+﻿getall();
+var isNew = true; // determine whether the current operation is for adding a new record or updating an existing if adding it bydefault true and if it updating it set false
+function addProject() {
+    var url = '';
+    var data = '';
+    var method;
+    // above variables will be used to store the URL, data, and HTTP method for the AJAX call.
+
+    if ($('#name').val() == '' || $('#fee').val() == '') {    //checking validation
+        alert('Please fill all required fields.');
+        return false;
+    }
+
+    if (isNew == true) {
+        url = '/lolc/save';
+        data = "{coursename:'" + $('#name').val() + "',fee:'" + $('#fee').val() + "'}";
+        method = 'POST';
+    }
+    else {
+        url = '/lolc/save';
+        data = "{coursename:'" + $('#name').val() + "',fee:'" + $('#fee').val() + "',id:'" + ID + "'}";
+        method = 'POST';
+    }
+
+    $.ajax({
+        type: method,
+        url: url,
+        dataType: 'JSON',
+        contentType: "application/json; charset=utf-8",
+        data: data,
+
+        success: function (data)
+        {
+            if (isNew) {
+                alert("Data Save successfully");
+              
+                getall();
+            }
+            else {
+                alert('Data updated successfully!');
+                window.location.reload();
+                getall();
+            }
+            $('#name').val('');
+            $('#fee').val('');
+        }
+    })
+}
+
+function getall() {
+    //destroy if any  existing data table before creating a new 
+    $('#tbl-school').dataTable().fnDestroy();
+    // Create a new data table using the DataTables plugin and Ajax to retrieve data from the server
+    $('#tbl-school').dataTable({
+        "ajax": {
+            "url": '/lolc/GetCourses',
+            "type": "GET",
+            "datatype": "JSON"
+        },
+        "columns": [
+            { data: "coursename" },
+            { data: "fee" },
+            {
+                data: "id", "render": function (data) {
+                    return '<button class="btn btn-success" onclick="get_course(' + data + ')">Edit</button>';
+                }
+            },
+            {
+
+                data: "id", "render": function (data) {
+                    return '<button class="btn btn-danger" onclick="get_delete(' + data + ')">Delete</button>';
+                }
+            }
+
+        ]
+    })
+
+}
+
+function get_course(id) {
+    $.ajax({
+        type: 'GET',
+        url: '/lolc/Edit?Id=' + id,
+        dataType: 'JSON',
+        contentType: "application/json; charset=utf-8",
+        success: function (data) {
+            isNew = false;
+            ID = data.id;
+            $('#name').val(data.coursename);
+            $('#fee').val(data.fee);
+            $('#save').text('Update');
+        }
+    });
+}
+
+function get_delete(id) {
+    $.ajax({
+        type: 'POST',
+        url: '/lolc/Remove?Id=' + id,
+        dataType: 'JSON',
+        contentType: "application/json; charset=utf-8",
+        success: function (data) {
+            alert("Delted")
+            getall();
+        }
+    });
+
+}
